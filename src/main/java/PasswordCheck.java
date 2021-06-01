4df2e0445300f0e6d9522f7c4398e3a2ac39f159
Petar <petar.vranesevic@gmail.com>
2021-06-02 00:54:27 +0200

    Alles ausser erste Erweiterung

--- src/main/java/PasswordCheck.java
+++ src/main/java/PasswordCheck.java
@@ -1,7 +1,48 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.lang.*;
+
 public class PasswordCheck {
 
 
-    public boolean checkPassword(String abc) {
-       return true;
+    public boolean checkPassword(String str) {
+        int num = str.length();
+        char[] list = new char[num];
+        for (int i = 0; i < num; i++) {
+            list[i] = str.charAt(i);
+        }
+        int duplicates = 0;
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[()#$?!%/@]).+$";
+        Pattern p = Pattern.compile(regex);
+
+        if (!(8 <= num && num <= 25)) {
+            return false;
+        }
+        // Upper, Lower, Numbers, Special
+        Matcher m = p.matcher(str);
+        if (!m.matches()) {
+            return false;
+        }
+
+        //Recurring
+        for (int i = 0; i < num; i++) {
+
+
+            for (int j = i + 1; j < num; j++) {
+                if (list[i] >= 48 && list[i] <= 57) {
+                    if (list[i] == list[j]) {
+                        duplicates++;
+                        if (duplicates > 3) return false;
+                    } else {
+                        duplicates = 0;
+                    }
+                }
+            }
+
+
+        }
+
+
+        return true;
     }
 }
--- src/test/java/PasswordCheckTest.java
+++ src/test/java/PasswordCheckTest.java
@@ -1,3 +1,4 @@
+import jdk.jfr.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,38 +8,77 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PasswordCheckTest  {
 
     @Test
-    @DisplayName("Is too short")
+    @DisplayName("Passwordlength")
+    @Description("Password too short")
     public void testPwLength1(){
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("abcdafg"));
+        assertFalse(pw.checkPassword("passwo"));
     }
 
 
     @Test
-    @DisplayName("Is too long")
+    @DisplayName("Passwordlength")
+    @Description("Password too long")
     public void testPwLength2(){
         PasswordCheck pw = new PasswordCheck();
         assertFalse(pw.checkPassword("ajkwposkindqiewoisunfiheaslkjh"));
     }
 
-
     @Test
-    @DisplayName("Is too short")
+    @DisplayName("All upper")
     public void testPwUpperLower1(){
         PasswordCheck pw = new PasswordCheck();
         assertFalse(pw.checkPassword("PASSWORD"));
     }
     @Test
-    @DisplayName("Is too short")
+    @DisplayName("All lower")
     public void testPwUpperLower2(){
         PasswordCheck pw = new PasswordCheck();
         assertFalse(pw.checkPassword("password"));
     }
 
     @Test
-    @DisplayName("Is too short")
-    public void testPwLength3(){
+    @DisplayName("At least one upper/lower")
+    public void testPwUpperLower3(){
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("abcdafg"));
+        assertTrue(pw.checkPassword("Password"));
     }
+    @Test
+    @DisplayName("At least one number")
+    public void testContainsNumber1(){
+        PasswordCheck pw = new PasswordCheck();
+        assertTrue(pw.checkPassword("Password1"));
+    }
+    @Test
+    @DisplayName("At least one number2")
+    public void testContainsNumber2(){
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.checkPassword("Password"));
+    }
+
+    @Test
+    @DisplayName("Has special char")
+    public void testHasSpecialchar(){
+        PasswordCheck pw = new PasswordCheck();
+        assertTrue(pw.checkPassword("Password12@"));
+    }
+    @Test
+    @DisplayName("Has special char2")
+    public void testHasSpecialchar2(){
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.checkPassword("Password12"));
+    }
+    @Test
+    @DisplayName("Recurring Numbers")
+    public void testRecurringNumbers1(){
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.checkPassword("Password@1111"));
+    }
+    @Test
+    @DisplayName("Recurring Numbers2")
+    public void testRecurringNumbers2(){
+        PasswordCheck pw = new PasswordCheck();
+        assertTrue(pw.checkPassword("Password111@"));
+    }
+
 }
