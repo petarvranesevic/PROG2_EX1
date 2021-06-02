5eb163801c2d6dc8271596e0b79ebc9536042566
Petar <petar.vranesevic@gmail.com>
2021-06-02 12:28:05 +0200

    Everything finished

--- src/main/java/PasswordCheck.java
+++ src/main/java/PasswordCheck.java
@@ -3,29 +3,61 @@ import java.util.regex.Pattern;
 import java.lang.*;
 
 public class PasswordCheck {
+    //"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[()#$?!%/@]).+$";
+    public String regexUL = "^(?=.*[A-Z])(?=.*[a-z]).+$";
+    public Pattern upper = Pattern.compile(regexUL);
+    public String regexNum = "^(?=.*\\d).+$";
+    public Pattern regNum = Pattern.compile(regexNum);
+    public String regexSpec = "^(?=.*[()#$?!%/@]).+$";
+    public Pattern special = Pattern.compile(regexSpec);
+
+public boolean checkPassword(String str){
+    return checkLength(str) && checkUpper(str) && checkNum(str) && checkSpecial(str) && checkRecurring(str) && checkNext(str);
+}
 
 
-    public boolean checkPassword(String str) {
+    public boolean checkLength(String str) {
         int num = str.length();
-        char[] list = new char[num];
-        for (int i = 0; i < num; i++) {
-            list[i] = str.charAt(i);
-        }
-        int duplicates = 0;
-        int counting = 0;
-
-
-        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[()#$?!%/@]).+$";
-        Pattern p = Pattern.compile(regex);
 
         if (!(8 <= num && num <= 25)) {
             return false;
         }
-        // Upper, Lower, Numbers, Special
-        Matcher m = p.matcher(str);
+        return true;
+//        char[] list = new char[num];
+//        for (int i = 0; i < num; i++) {
+//            list[i] = str.charAt(i);
+//        }
+
+
+    }
+
+    public boolean checkUpper(String str) {
+        // Upper, Lower
+        Matcher m = upper.matcher(str);
         if (!m.matches()) {
             return false;
         }
+        return true;
+    }
+    public boolean checkNum(String str) {
+        // Numbers
+        Matcher m = regNum.matcher(str);
+        if (!m.matches()) {
+            return false;
+        }
+        return true;
+    }
+    public boolean checkSpecial(String str) {
+        // Numbers
+        Matcher m = special.matcher(str);
+        if (!m.matches()) {
+            return false;
+        }
+        return true;
+    }
+
+
+
 /*
         //Counting
         for (int i = 0; i < num; i++) {
@@ -41,21 +73,48 @@ public class PasswordCheck {
             }
         }
 */
+public boolean checkNext(String str) {
+    int first = 0, second = 0, third = 0;
+    
+    for (int i = 0; i <= str.length(); i++) {
+        if (i < str.length()-2 && Character.isDigit(str.charAt(i)) && Character.isDigit(str.charAt(i+1)) && Character.isDigit(str.charAt(i+2))){
+            first = str.charAt(i);
+            second = str.charAt(i+1);
+            third = str.charAt(i+2);
+        }
+    }
+    if (first == second-1 && first == third-2) {
+        return false;
+    }
 
-        //Recurring
-        for (int i = 0; i < num; i++) {
-            for (int j = i + 1; j < num; j++) {
-                if (list[i] >= 48 && list[i] <= 57) {
-                    if (list[i] == list[j]) {
-                        duplicates++;
-                        if (duplicates > 3) return false;
-                    } else {
-                        duplicates = 0;
-                    }
+    return true;
+}
+
+    public boolean checkRecurring(String str) {
+        int first = 0;
+        int second = 0;
+        int third = 0;
+        int fourth = 0;
+
+
+        for (int i = 0; i <= str.length(); i++) {
+            if (i < str.length()-3 && Character.isDigit(str.charAt(i)) && Character.isDigit(str.charAt(i+1)) &&
+                    Character.isDigit(str.charAt(i+2)) && Character.isDigit(str.charAt(i+3))){
+
+                first = str.charAt(i);
+                second = str.charAt(i+1);
+                third = str.charAt(i+2);
+                fourth = str.charAt(i+3);
+
+                if (first == second && first == third && first == fourth) {
+                    return false;
                 }
             }
         }
-
         return true;
+
     }
+
+
 }
+
--- src/test/java/PasswordCheckTest.java
+++ src/test/java/PasswordCheckTest.java
@@ -5,94 +5,141 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PasswordCheckTest  {
+public class PasswordCheckTest {
+    /*
+    Checking Correct Password length 8-25
+     */
 
     @Test
     @DisplayName("Passwordlength")
     @Description("Password too short")
-    public void testPwLength1(){
+    public void testPwLength1() {
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("passwo"));
+        assertFalse(pw.checkLength("passwo"));
     }
-
 
     @Test
     @DisplayName("Passwordlength")
     @Description("Password too long")
-    public void testPwLength2(){
+    public void testPwLength2() {
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("ajkwposkindqiewoisunfiheaslkjh"));
+        assertFalse(pw.checkLength("ajkwposkindqiewoisunfiheaslkjh"));
     }
 
     @Test
-    @DisplayName("All upper")
-    public void testPwUpperLower1(){
+    @DisplayName("Passwordlength")
+    @Description("Password right size")
+    public void testPwLength3() {
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("PASSWORD"));
+        assertTrue(pw.checkLength("password"));
     }
+
+
+
+    /*
+    Checking if there is at least one upper and lowercase letter
+     */
+
     @Test
-    @DisplayName("All lower")
-    public void testPwUpperLower2(){
+    @DisplayName("Uppercase/Lowercase")
+    public void testPwUpperLower1() {
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("password"));
+        assertFalse(pw.checkUpper("PASSWORD"));
     }
 
     @Test
-    @DisplayName("At least one upper/lower")
-    public void testPwUpperLower3(){
+    @DisplayName("Uppercase/Lowercase")
+    public void testPwUpperLower2() {
         PasswordCheck pw = new PasswordCheck();
-        assertTrue(pw.checkPassword("Password"));
-    }
-    @Test
-    @DisplayName("At least one number")
-    public void testContainsNumber1(){
-        PasswordCheck pw = new PasswordCheck();
-        assertTrue(pw.checkPassword("Password1"));
-    }
-    @Test
-    @DisplayName("At least one number2")
-    public void testContainsNumber2(){
-        PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("Password"));
+        assertFalse(pw.checkUpper("password"));
     }
 
     @Test
-    @DisplayName("Has special char")
-    public void testHasSpecialchar(){
+    @DisplayName("Uppercase/Lowercase")
+    public void testPwUpperLower3() {
         PasswordCheck pw = new PasswordCheck();
-        assertTrue(pw.checkPassword("Password12@"));
-    }
-    @Test
-    @DisplayName("Has special char2")
-    public void testHasSpecialchar2(){
-        PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("Password12"));
-    }
-    @Test
-    @DisplayName("Recurring Numbers")
-    public void testRecurringNumbers1(){
-        PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("Password@1111"));
-    }
-    @Test
-    @DisplayName("Recurring Numbers2")
-    public void testRecurringNumbers2(){
-        PasswordCheck pw = new PasswordCheck();
-        assertTrue(pw.checkPassword("Password111@"));
-    }
-/*
-    @Test
-    @DisplayName("Counting Numbers")
-    public void testCountingNumbers(){
-        PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.checkPassword("Password123@"));
-    }
-    @Test
-    @DisplayName("Counting2")
-    public void testCountingNumbers2(){
-        PasswordCheck pw = new PasswordCheck();
-        assertTrue(pw.checkPassword("Password111@"));
+        assertTrue(pw.checkUpper("PassWord"));
     }
 
- */
+
+
+    /*
+    Checking if there is at least one number
+     */
+
+    @Test
+    @DisplayName("At least one number 1")
+    public void testContainsNumber1() {
+        PasswordCheck pw = new PasswordCheck();
+        assertTrue(pw.checkNum("Password1"));
+    }
+
+    @Test
+    @DisplayName("At least one number 2")
+    public void testContainsNumber2() {
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.checkNum("Password"));
+    }
+
+
+
+    /*
+    Checking if there is at least one special char
+     */
+
+    @Test
+    @DisplayName("At least one special char 1")
+    public void testHasSpecialchar() {
+        PasswordCheck pw = new PasswordCheck();
+        assertTrue(pw.checkSpecial("Password12@"));
+    }
+
+    @Test
+    @DisplayName("At least one special char 2")
+    public void testHasSpecialchar2() {
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.checkSpecial("Password12"));
+    }
+
+
+
+    /*
+    Checking if 3 numbers are not ascending
+    e.g 123 = false 124 = true
+     */
+
+    @Test
+    @DisplayName("Ascending numbers 1")
+    public void testCountingNumbers() {
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.checkNext("Password123@"));
+    }
+
+    @Test
+    @DisplayName("Ascending numbers 2")
+    public void testCountingNumbers2() {
+        PasswordCheck pw = new PasswordCheck();
+        assertTrue(pw.checkNext("Password111@"));
+    }
+
+
+
+    /*
+    Checking if more than 3 numbers are next to each other
+    */
+
+    @Test
+    @DisplayName("Recurring Numbers 1")
+    public void testRecurringNumbers1() {
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.checkRecurring("Password@1111"));
+    }
+
+    @Test
+    @DisplayName("Recurring Numbers 2")
+    public void testRecurringNumbers2() {
+        PasswordCheck pw = new PasswordCheck();
+        assertTrue(pw.checkRecurring("Password111@"));
+    }
+
 }
